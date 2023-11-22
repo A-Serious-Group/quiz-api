2df2d1f17424f9355d92579ab49e2bfb569671fa
NbMiranda <nathanbmiranda@hotmail.com>
2023-11-21 21:43:28 -0300

    changes

--- src/app/queezy/queezy_user/queezy_user.controller.ts
+++ src/app/queezy/queezy_user/queezy_user.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete,HttpCode } from '@nestjs/common';
 import { QueezyUserService } from './queezy_user.service';
 import { UpdateQueezyUserDto } from './dto/update-queezy_user.dto';
-import { ApiBody, ApiHideProperty, ApiOperation, ApiProduces, ApiProperty, ApiTags } from '@nestjs/swagger';
+import { ApiTags } from '@nestjs/swagger';
 
 @ApiTags('Users')
 @Controller('queezy')
@@ -22,9 +22,8 @@ export class QueezyUserController {
     return this.queezyUserService.findOne(+id);
   }
 
-  @ApiOperation({summary: 'Alterar usuário por id', description: "Altera o usuário pelo id fornecido"})
-  @Patch('/api/queezy/user/:id_user')
-  update(@Param('id_user') id: string, @Body() updateQueezyUserDto: UpdateQueezyUserDto) {
+  @Patch('/api/queezy/user/:id')
+  update(@Param('id') id: string, @Body() updateQueezyUserDto: UpdateQueezyUserDto) {
     return this.queezyUserService.update(+id, updateQueezyUserDto);
   }
 
